9135bb30b8df4fada401b3e5e3e1606d8d3fb031
MakiforDevelop <makifordevelop@gmail.com>
2025-05-27 11:15:31 +0800

    Initial Commit

---
+++ NekoSubs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NekoSubs
+//
+//  Created by 千葉牧人 on 2025/5/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NekoSubs/NekoSubsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekoSubsApp.swift
+//  NekoSubs
+//
+//  Created by 千葉牧人 on 2025/5/27.
+//
+
+import SwiftUI
+
+@main
+struct NekoSubsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
